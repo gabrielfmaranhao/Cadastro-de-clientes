e62b5b03d78dbce5bd4dc3b235382e498c228047
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2023-02-09 17:32:52 -0300

    feat: new route doc

--- _core/src/routes/index.ts
+++ _core/src/routes/index.ts
@@ -1,9 +1,11 @@
 import sessionRoutes from "./session.routes";
 import clientRoutes from "./client.routes";
 import { Express } from "express";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../swagger.json";
 const appRoutes = ( app: Express) => {
     app.use("/api/user/", sessionRoutes)
     app.use("/api/cliente/", clientRoutes)
+    app.use("/api/docs/", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
 export default appRoutes
